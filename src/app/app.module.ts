9700a55cc6572f810f5c09498358382af3cc4f86
SeymaDemir <seymademir35@gmail.com>
2023-08-17 17:25:40 +0300

    finished homepage with responsive design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './app/components/homepage/homepage.component';
 import { LoginpageComponent } from './components/loginpage/loginpage.component';
 import { RegisterpageComponent } from './components/registerpage/registerpage.component';
 import { ProfilepageComponent } from './components/profilepage/profilepage.component';
 import { CvdetailpageComponent } from './components/cvdetailpage/cvdetailpage.component';
 import { CreatecvpageComponent } from './components/createcvpage/createcvpage.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
 @NgModule({
   declarations: [
