4d873e3b2822664c39854f04a9681c68af61a002
SeymaDemir <seymademir35@gmail.com>
2023-08-18 21:28:36 +0300

    added validation to login page form

--- src/app/components/loginpage/loginpage.component.ts
+++ src/app/components/loginpage/loginpage.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'; 
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-loginpage',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class LoginpageComponent {
 
+  registerForm: FormGroup; 
+  title='formvalidation';
+  submitted=false;
+  
+  constructor(private formBuilder:FormBuilder, private router: Router){}
+
+  ngOnInit(){
+    // validation rules
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    })
+  }
+
+  onSubmit(){
+    this.submitted = true
+
+    if(this.registerForm.invalid){
+      return
+    }
+    else{this.router.navigate(['/profile']); }
+  }
 }
